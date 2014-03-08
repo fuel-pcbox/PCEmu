013eb5133f336263c6ac02c03c875e82673dd3b0
Darius Goad <alegend45@gmail.com>
2014-03-08 09:12:52 -0600

    First commit

---
+++ common.h
@@ -0,0 +1,16 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <cstdio>
+#include <cstdint>
+
+typedef uint8_t u8;
+typedef int8_t s8;
+typedef uint16_t u16;
+typedef int16_t s16;
+typedef uint32_t u32;
+typedef int32_t s32;
+
+#define log(...) printf("%s:%s %s", __FILE__, __LINE__, __VA_ARGS__)
+
+#endif
